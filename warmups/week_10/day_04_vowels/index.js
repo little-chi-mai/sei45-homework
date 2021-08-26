bc17cb55ce20ceef7c6545b5fb2cbfb3e6758e52
Misael M <phatarino@gmail.com>
2021-08-26 10:16:21 +1000

    vowels warmup

---
+++ warmups/week_10/day_04_vowels/index.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+//    for (let char of str.toLowerCase()) {
+//      if (checker.includes(char)) {
+//        count++
+//      }
+//    }
+//    return count;
+// }
+// console.log(vowels('water'));
+
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return console.log(matches ? matches.length : 0);
+}
+
+vowels('superman');
